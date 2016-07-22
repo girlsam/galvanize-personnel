c1de27e55ebb75a9ffae9612a5bbf10d184ecc5e
Sam Goldsmith <samggold@gmail.com>
2016-07-22 15:48:46 -0600

    initial commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,19 @@
-// Your code here!
+$(document).ready(function(){
+  console.log('sanity check');
+  $.ajax({
+    url: 'http://galvanize-student-apis.herokuapp.com/gpersonnel/roles',
+    method: 'GET'
+  }).done(function(data){
+    for (i = 0; i < data.length; i++) {
+      var titleVal = data[i].title;
+      $('select').append('<option value=' + titleVal + '>' + titleVal + '</option>');
+      }
+      $('select').change(function() {
+        data.forEach(function(role) {
+        if (role.title === $('#role').val()) {
+          $('img').attr('src','.' + role.img);
+          }
+        });
+      });
+  });
+});
