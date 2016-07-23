21e8c6aabe3379b7822118858a101c9bea14e91c
Sam Goldsmith <samggold@gmail.com>
2016-07-23 14:48:23 -0600

    ready for push to site

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  console.log('sanity check');
   $.ajax({
     url: 'http://galvanize-student-apis.herokuapp.com/gpersonnel/roles',
     method: 'GET'
@@ -15,5 +14,28 @@ $(document).ready(function(){
           }
         });
       });
+    });
+  $('button').on('click', function(event) {
+    event.preventDefault();
+    var fName = $('#firstName').val();
+    var lName = $('#lastName').val();
+    var selectedRole = $('#role').val();
+    $.ajax({
+      url: 'http://galvanize-student-apis.herokuapp.com/gpersonnel/users',
+      method: 'POST',
+      data: {firstName: fName, lastName: lName, role: selectedRole}
+    }).done(function(results) {
+      var successMessage = results.message;
+      $('.save-status').fadeOut(0);
+      $('.save-status').append('<p class="alert alert-dismissible alert-success">' + successMessage + '</p>');
+      $('.save-status').fadeIn(500).delay(2000).fadeOut(500);
+    }).fail(function(error) {
+      var failMessage = error.responseJSON.message;
+      $('.save-status').fadeOut(0);
+      $('.save-status').append('<p class="alert alert-dismissible alert-danger">' + failMessage + '</p>');
+      $('.save-status').fadeIn(500, function() {
+        $(this).delay(2000).fadeOut(500);
+      });
+    });
   });
 });
